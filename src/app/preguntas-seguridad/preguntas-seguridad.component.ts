a2704850b46cc1de78c2d330cf90cebf20df0b90
Ange-G <69248339+Ange-G@users.noreply.github.com>
2020-10-04 23:48:42 -0500

    NgOnInit

--- src/app/preguntas-seguridad/preguntas-seguridad.component.ts
+++ src/app/preguntas-seguridad/preguntas-seguridad.component.ts
@@ -16,19 +16,30 @@ export class PreguntasSeguridadComponent implements OnInit {
   perfiles: any;
   nombreC: any;
   claveCliente: string;
+  roles:any;
 
+  constructor(private db:AngularFireDatabase, private router: Router, private ActivatedRoute: ActivatedRoute) {
 
-  constructor(db:AngularFireDatabase, private router: Router, private ActivatedRoute: ActivatedRoute) {
+    
+    
 
+    
 
-    this.ActivatedRoute.paramMap.subscribe((parametros: ParamMap) => {
+  }
+//ruta dinamica
+
+ngOnInit (): void  {
+  /* this.claveCliente = this.route.snapshot.paramMap.get('claveCliente'); */  
+  this.ActivatedRoute.paramMap.subscribe((parametros: ParamMap) => {
     this.claveCliente = String(parametros.get("claveCliente"));
     console.log("clave cliente",this.claveCliente)
+    this.obtenerDatos(this.db, this.claveCliente);
     } )
+    
+  }
 
-    console.log("constructor", this.claveCliente);
-
-    db.object(`/clientes/${this.claveCliente}/datosCliente`)
+  obtenerDatos(db, claveCliente): void{
+     db.object(`/clientes/${claveCliente}/datosCliente`)
     .valueChanges()
     .subscribe(clientes=> {
     this.clientes= clientes;
@@ -36,13 +47,13 @@ export class PreguntasSeguridadComponent implements OnInit {
   });
 
 
-    db.object(`/clientes/${this.claveCliente}/datosPoliza`)
+    db.object(`/clientes/${claveCliente}/datosPoliza`)
     .valueChanges()
     .subscribe(poliza=> {
     this.poliza= poliza;
     console.log(this.poliza);
   });
-    db.object(`/clientes/${this.claveCliente}/datosCliente/nombreCompleto`)
+    db.object(`/clientes/${claveCliente}/datosCliente/nombreCompleto`)
     .valueChanges()
     .subscribe(nombreC=> {
     this.nombreC= nombreC;
@@ -62,14 +73,15 @@ export class PreguntasSeguridadComponent implements OnInit {
     this.perfiles= perfiles;
     console.log(this.perfiles);
   });
-
-}
-//ruta dinamica
-
-ngOnInit (): void  {
-  /* this.claveCliente = this.route.snapshot.paramMap.get('claveCliente'); */
+    db.object(`/roles`)
+    .valueChanges()
+    .subscribe(roles=> {
+    this.roles= roles;
+    console.log(this.roles);
+  });
 
   }
+
 
 //Ng Zorro
   isVisible = false;
